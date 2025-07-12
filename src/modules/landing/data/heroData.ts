48a609ddbf8804506b6e3c15de477d762b688834
aungkokothet <aungkokothet@gmail.com>
2025-07-12 03:41:31 +0000

    text

--- src/modules/landing/data/heroData.ts
+++ src/modules/landing/data/heroData.ts
@@ -1,7 +1,7 @@
 export const heroData = {
   title: "The Builders’ Stack",
   subtitle:
-    "Uncovering the Hidden in App Development",
+    "Uncovering the Hidden Leverage in App Development",
   text1: "The Builders' Stack is A 15-minute talk show series uncovering what’s hidden in your app journey powered by the tools and teams that help you build, launch, and grow.",
   text2: "It is about uncovering the hidden structures, patterns, and processes in your business that you could leverage on for your accelerated growth and building your Apps from there.",
 }
